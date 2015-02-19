61c2d36084a56a0032f96d5e0e33b9714fd235f9
Chris Loew <cloew123@gmail.com>
2015-02-19 15:48:40 -0700

    Added core Kao Config functionality to find global and local configuration files

---
+++ src/kao_config/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+def GetUserHomeFolder():
+    """ Return the User's Home Directory """
+    return os.path.expanduser("~")
+    
+def GetGlobalDirectory(directory):
+    """ Return the Global Directory """
+    return os.path.join(GetUserHomeFolder(), directory)
+    
+class LocalConfigFinder:
+    """ Class to help find a local config file """
+    
+    def __init__(self, filename):
+        """ Initialize the local config finder with the file to search for """
+        self.filename = filename
+        self.fullPath = None
+        
+    def find(self, startFrom=None):
+        """ Return the path to the closest file by searching the startFrom 
+            directory or the current directory up to the file root """
+        if self.fullPath is not None:
+            return self.fullPath
+            
+        if startFrom is None:
+            currentDirectory = os.getcwd()
+        else:
+            currentDirectory = startFrom
+    
+        self.fullPath = self.search(currentDirectory)
+        return self.fullPath
+        
+    def search(self, currentDirectory):
+        """ Search for the file """
+        while currentDirectory != '/':
+            fullFilename = os.path.join(currentDirectory, self.filename)
+            if os.path.exists(fullFilename):
+                return fullFilename
+            currentDirectory = os.path.dirname(currentDirectory)
