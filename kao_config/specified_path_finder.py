a3adaba24da512a0b3afc92346c28ac6c9ecfc2e
Chris Loew <cloew123@gmail.com>
2015-09-27 18:29:26 -0600

    Added a finder to find the exact path given

---
+++ kao_config/specified_path_finder.py
@@ -0,0 +1,7 @@
+
+class SpecifiedPathFinder:
+    """ Represents a method of fidning a File at exactly the path specified """
+    
+    def __init__(self, filename):
+        """ Initialize with the filename to load """
+        self.path = filename
